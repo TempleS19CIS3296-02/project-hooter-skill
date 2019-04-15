e45e10ef63fab50064e944d2cf91a15087a2eeb0
Viet Pham <tug36669@temple.edu>
2019-04-14 20:16:28 -0400

    Refactored eventLookUp

--- hooter_endpoint/handlers/eventsLookUpHandler.js
+++ hooter_endpoint/handlers/eventsLookUpHandler.js
@@ -1,38 +1,48 @@
+"use strict";
 var { google } = require('googleapis')
-var scopes = ['https://www.googleapis.com/auth/calendar.readonly']
 var key = require('../HooterSkill-5fde0850cf41.json'); // private json
-var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, scopes, null);
+
 
 const eventsLookUpHandler = {
-    "EventsLookUpIntent": function () {
-        var speechOutput = "";
+    "EventsLookUpIntent": async function () {
+        var scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+        var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, scopes, null );
 
-        var auth = jwtClient.authorize(function (err, token) {
-            listCalendars(jwtClient);
-        });
-
-        const calendar = google.calendar({ version: 'v3', auth });
-        calendar.events.list({
-            calendarId: 'hooterstella@gmail.com',
-            timeMin: (new Date()).toISOString(),
-            maxResults: 5,
-            singleEvents: true,
-            orderBy: 'startTime',
-
+        jwtClient.authorize();
+        var auth = jwtClient;
+        if (this.event.request.intent.slots.dateoftheweek.value){
+            var userinputdate = this.event.request.intent.slots.dateoftheweek.value;
+        } else {
+            var userinputdate = (new Date()).getDate();
+        }
+     
+        const calendar = google.calendar({version: 'v3', auth});
+            calendar.events.list({
+                calendarId: 'hooterstella@gmail.com',
+                timeMin: (new Date(userinputdate)).toISOString(),
+                maxResults: 3,
+                singleEvents: true,
+                orderBy: 'startTime'
         }, (err, res) => {
-            if (err) speechOutput += "Error retreiving events";
+            var speechOutput = "";
+            if (err) return console.log('The API returned an error: ' + err);
             const events = res.data.items;
             if (events.length) {
-                speechOutput += 'Upcoming 5 events:';
+                speechOutput += 'Upcoming events: ';
                 events.map((event, i) => {
-                    const start = event.start.dateTime || event.start.date;
-                    speechOutput += `${start} - ${event.summary}`;
+                    const start = new Date(event.start.dateTime || event.start.date);
+                    const end = new Date(event.end.dateTime || event.end.date);
+                    var startDate = (start.getMonth()+1).toString() + "/" + start.getDate() + "/" + start.getFullYear();
+                    //var endDate = end.getMonth().toString() + "/" + end.getDate() + "/" + end.getFullYear();
+                    var startHour = (start.getHours()).toString() + ":" + (start.getMinutes()).toString();
+                    var endHour = (end.getHours()).toString() + ":" + (end.getMinutes()).toString();
+                    speechOutput += ` ${startDate} - ${event.summary} at ${startHour}`;
                 });
             } else {
                 speechOutput += 'No upcoming events found.';
             }
+            this.emit(':tell',speechOutput);
         });
-        this.emit(":tell", speechOutput);
     }
 }
 
