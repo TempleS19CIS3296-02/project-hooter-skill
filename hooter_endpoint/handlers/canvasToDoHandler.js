f7b2f674a3533100b0db030f6c017ea1a06eb24c
Sean <SeanR1194@gmail.com>
2019-04-16 22:56:52 -0400

    added new icons

--- hooter_endpoint/handlers/canvasToDoHandler.js
+++ hooter_endpoint/handlers/canvasToDoHandler.js
@@ -2,37 +2,44 @@
 const getAccessToken = require("../canvasUtils/getAccessToken.js");
 const getToDo = require("../canvasUtils/getToDo.js");
 const getCourses = require("../canvasUtils/getCourses.js");
+const Alexa = require("alexa-sdk");
 
 const canvasToDoHandler = {
     "CanvasToDoIntent": async function () {
         var speechOutput = "";
         const alexa = this;
-        try {
-            // Read manually generated access token from local file
-            const AUTH_TOKEN = getAccessToken.getAccessToken();
+        speechOutput = "hello world!";
 
-            // Get courses from Canvas API
-            speechOutput = await getCourses.getCourses(AUTH_TOKEN)
-                // .then((courseMap) => {
-                .then(async function (courseMap) {
-                    try {
-                        // Get to do list from Canvas API
-                        return await getToDo.getToDo(AUTH_TOKEN, courseMap)
-                            .then((out) => {
-                                // Output speech formatted to do list
-                                return out;
-                            });
-                    } catch (error) {
-                        console.error(error);
-                        alexa.emit(":tell", "An error occured");
-                    }
-                });
-        } catch (error) {
-            console.error(error);
-            alexa.emit(":tell", "An error occured");
-        }
-        console.log(speechOutput);
-        // alexa.emit(":tell", speechOutput);
+        // try {
+        //     // Read manually generated access token from local file
+        //     const AUTH_TOKEN = getAccessToken.getAccessToken();
+
+        //     // Get courses from Canvas API
+        //     speechOutput = await getCourses.getCourses(AUTH_TOKEN)
+        //         // .then((courseMap) => {
+        //         .then(async function (courseMap) {
+        //             try {
+        //                 // Get to do list from Canvas API
+        //                 return await getToDo.getToDo(AUTH_TOKEN, courseMap)
+        //                     .then((out) => {
+        //                         // Output speech formatted to do list
+        //                         return out;
+        //                     });
+        //             } catch (error) {
+        //                 console.error(error);
+        //                 alexa.emit(":tell", "An error occured");
+        //             }
+        //         });
+        // } catch (error) {
+        //     console.error(error);
+        //     alexa.emit(":tell", "An error occured");
+        // }
+        // console.log(speechOutput);
+
+
+        // this.response.speak(speechOutput);
+        // this.emit(":responseReady");
+        this.emit(":tell", speechOutput);
     } //end CanvasToDoIntent()
 } // end canvasToDoHandler
 
