5342ac6c345d00652203ca2bf9b974a9c1beb3dd
Viet Pham <tug36669@temple.edu>
2019-04-27 11:12:08 -0400

    try shouldendsession

--- hooter_endpoint/handlers/eventsLookUpHandler.js
+++ hooter_endpoint/handlers/eventsLookUpHandler.js
@@ -3,6 +3,8 @@ var { google } = require("googleapis");
 var key = require("../HooterSkill-5fde0850cf41.json"); // private json
 const reprompt = "What can I help you with?";
 var AmazonSpeech = require("ssml-builder/amazon_speech");
+var cardTitle = "Your upcoming event:";
+var cardContent = "";
 var imageObj = {
   smallImageUrl: "https://i.imgur.com/0lpxVh6.png", //108x108
   largeImageUrl: "https://i.imgur.com/QIq2lcs.png" //240x240
@@ -78,10 +80,16 @@ const eventsLookUpHandler = {
           speechOutput +=
             "I can't find the event " + eventname + ". Please try again!";
         }
-        this.emit(":tellWithCard", speechOutput);
-        this.emit(":responseReady");
-
-        // this.emit(":tellWithCard", speechOutput, cardTitle, cardContent, imageObj);
+        cardContent += speechOutput;
+        this.emit(
+          ":tellWithCard",
+          speechOutput,
+          cardTitle,
+          cardContent,
+          imageObj
+        );
+        this.response.shouldEndSession = false;
+        this.response.listen();
       }
     );
   }
--- hooter_endpoint/index.js
+++ hooter_endpoint/index.js
@@ -13,10 +13,10 @@ const APP_ID = "amzn1.ask.skill.e896779a-a041-473e-9834-95af9e94d2f4";
 // const newSession = require("./handlers/newSession"); // used for testing
 const defaultHandlers = require("./handlers/defaultHandlers.js");
 const hoursLookUpHandler = require("./handlers/hoursLookUpHandler.js");
-const distanceLookUpHandler = require("./handlers/distanceLookUpHandler.js");
+//const distanceLookUpHandler = require("./handlers/distanceLookUpHandler.js");
 const eventsLookUpHandler = require("./handlers/eventsLookUpHandler.js");
-const canvasToDoHandler = require("./handlers/canvasToDoHandler.js");
-const directionsLookUpHandler = require("./handlers/directionsLookUpHandler.js");
+//const canvasToDoHandler = require("./handlers/canvasToDoHandler.js");
+//const directionsLookUpHandler = require("./handlers/directionsLookUpHandler.js");
 
 canvasToDoHandler.CanvasToDoIntent();
 
@@ -26,10 +26,10 @@ exports.handler = function(event, context, callback) {
   alexa.registerHandlers(
     defaultHandlers,
     hoursLookUpHandler,
-    distanceLookUpHandler,
-    eventsLookUpHandler,
-    canvasToDoHandler,
-    directionsLookUpHandler
+    //distanceLookUpHandler,
+    eventsLookUpHandler
+    //canvasToDoHandler,
+    //directionsLookUpHandler
   );
   alexa.execute();
 };
